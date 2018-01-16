0b2dc1ae12bebcad20b349dcdb3e551031001881
Daria Voytova <vdariaclem@gmail.com>
2018-01-16 11:09:12 +0300

    Add a new topic to collection

--- apinf_packages/mqtt_dashboard/client/acl_page.js
+++ apinf_packages/mqtt_dashboard/client/acl_page.js
@@ -3,11 +3,21 @@
   You may obtain a copy of the licence at
   https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
+import AclRules from '../collection/index';
+
+Template.aclPage.onCreated(function () {
+  this.subscribe('allAclRules');
+});
+
 Template.aclPage.events({
   'submit form': (event, templateInstance) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const topicValue = formData.get('topic');
 
+    AclRules.insert({
+      topic: topicValue,
+      starred: false,
+    });
   },
 });
